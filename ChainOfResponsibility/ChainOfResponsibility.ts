26fa56ad78070e661d90c7127420a82a6acaeeb2
bu <86216382+hindong@users.noreply.github.com>
2023-04-13 20:33:08 +0900

    Update ChainOfResponsibility.ts

--- ChainOfResponsibility/ChainOfResponsibility.ts
+++ ChainOfResponsibility/ChainOfResponsibility.ts
@@ -1,1 +1,13 @@
+interface Handler{
+    setNext(handler: Handler): Handler;
 
+    handle(request: string): string;
+}
+
+abstract class AbstractHandler implements Handler{
+    private nextHandler: Handler;
+
+    public setNext(handler: Handler): Handler {
+        
+    }
+}
