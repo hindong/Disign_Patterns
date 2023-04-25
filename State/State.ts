08c6e3c79eabd57c5409b671e5ce29f775b4b7cd
bu <86216382+hindong@users.noreply.github.com>
2023-04-26 01:13:43 +0900

    Update State.ts

--- State/State.ts
+++ State/State.ts
@@ -1,1 +1,34 @@
 
+class Context{
+    private state: State;
+
+    constructor(state: State){
+        this.transition(state);
+    }
+
+    public transition(state: State): void{
+        console.log(`Context: Transition to ${(<any>state).constructor.name}.`);
+        this.state = state;
+        this.state.setContext(this);
+    }
+
+    public request1(): void{
+        this.state.handle1();
+    }
+
+    public request2(): void{
+        this.state.handle2();
+    }
+}
+
+abstract class State{
+    protected context: Context;
+
+    public setContext(context: Context){
+        this.context = context;
+    }
+
+    public abstract handle1(): void;
+
+    public abstract handle2(): void;
+}
